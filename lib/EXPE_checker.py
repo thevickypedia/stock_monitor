71358e411e8e4141e1ebf1b17c574f4a61787b9e
Vignesh <38729644+vignesh1793@users.noreply.github.com>
2020-04-25 09:35:09 -0500

    Initial commit

---
+++ lib/EXPE_checker.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+import os
+import re
+from datetime import datetime
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+stock = 'EXPE'
+stock_price = os.getenv('EXPE')
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+
+class EXPEChecker:
+    def statistics(self):
+        global result, diff
+        r = requests.get(f'https://finance.yahoo.com/quote/{stock}')
+        scrapped = bs(r.text, "html.parser")
+        raw_data = scrapped.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0]
+        price = raw_data.find('span').text
+        percent = re.findall(r"\d+\.\d+", str(raw_data))
+
+        if 'tertiaryColor' in str(raw_data) and len(percent) > 5:
+            # print('After Market Hours\nCheck for yourself lazy ASS')
+            result = 'currently no change. Last change:'
+            # sys.exit()
+        elif 'negativeColor' in str(raw_data):
+            result = 'decreased'
+        elif 'positiveColor' in str(raw_data):
+            result = 'increased'
+        diff = round(float(price) - float(stock_price), 2)
+        msg = f"The current price of {stock} is: ${price}\nYou bought it for: ${stock_price}\nDifference: " \
+              f"{diff}\n{stock} share has {result} {percent[-1]}%"
+        if diff < 0:
+            message = f'Time to buy new shares.\n\nData as of {dt_string}:\n{msg}\n'
+            return message
---
+++ stock_monitor.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+import os
+
+from lib.DFS_checker import DFSChecker
+from lib.EXPE_checker import EXPEChecker
+from lib.RCL_checker import RCLChecker
+from lib.UAL_checker import UALChecker
+from lib.emailer import Emailer
+
+
+def email_formatter():
+    UAL_info = UALChecker().statistics()
+    EXPE_info = EXPEChecker().statistics()
+    RCL_info = RCLChecker().statistics()
+    DFS_info = DFSChecker().statistics()
+
+    email_text = 'Stock Monitoring Notification\n'
+
+    if UAL_info:
+        email_text += '\nUnited Airlines Report:\n'
+        email_text += UAL_info
+
+    if EXPE_info:
+        email_text += '\nExpedia Report:\n'
+        email_text += EXPE_info
+
+    if RCL_info:
+        email_text += '\nRoyal Caribbean Report:\n'
+        email_text += RCL_info
+
+    if DFS_info:
+        email_text += '\nDiscover Report:\n'
+        email_text += DFS_info
+
+    return email_text
+
+
+if __name__ == '__main__':
+    sender_env = os.getenv('SEND')
+    recipient_env = os.getenv('RECIPIENT')
+    git_env = os.getenv('GIT')
+    footer_text = "\n----------------------------------------------------------------" \
+                  "----------------------------------------\n" \
+                  "A report on the list shares you have purchased. The data is being collected from https://finance.yahoo.com, " \
+                  f"\nFor more information check README.md in {git_env}"
+    sender = f'Stock Hawk <{sender_env}>'
+    recipient = [f'{recipient_env}']
+    title = 'Stock Monitor Alert'
+    text = f'{email_formatter()}\n\n{footer_text}'
+    Emailer(sender, recipient, title, text)
